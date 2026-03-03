a7f79c2859e7222641f573c95ba6ab4abd78d784
Mahedi Hasan Jihad <mahdijihad001@gmail.com>
2026-03-03 12:18:25 +0600

    Fix Cors error

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,19 @@ async function bootstrap() {
   app.useWebSocketAdapter(new IoAdapter(app));
 
   // Enable CORS
+  const allowedOrigins = [
+    'http://localhost:3000',
+    `https://www.inkleinelevators.com`
+  ];
+
   app.enableCors({
-    origin: process.env.FRONTEND_URL || '*',
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
